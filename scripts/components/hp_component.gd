ca44bee0f3c227df60140ce0d8180b0bcba33af4
Polar4ik <atossnikitoss@gmail.com>
2024-08-28 18:51:43 +0400

    add die

--- scripts/components/hp_component.gd
+++ scripts/components/hp_component.gd
@@ -1,6 +1,8 @@
 extends Node
 class_name HpComponent
 
+signal die
+
 @export var MAX_HP := 100.0
 var hp := 0.0
 
@@ -10,6 +12,9 @@ func _ready() -> void:
 func use_hp(count: float) -> void:
 	hp -= count
 	hp = clampf(hp, 0.0, MAX_HP)
+	
+	if hp == 0.0:
+		die.emit()
 
 func restore_hp(count: float) -> void:
 	hp += count
